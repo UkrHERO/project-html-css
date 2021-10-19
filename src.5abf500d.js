40a3f0477990a777f96d1c2fb5a98265ba74cf95
UkrHERO <89246358+UkrHERO@users.noreply.github.com>
2021-10-19 09:15:55 +0000

    Deploying to gh-pages from @ UkrHERO/project-html-css@62adbe5b1b6a63d451ea07b3f54292241525ce1e 🚀

--- src.5abf500d.js
+++ src.5abf500d.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/project-html-css/src.b4958b1d.js.map
+//# sourceMappingURL=/project-html-css/src.5abf500d.js.map
